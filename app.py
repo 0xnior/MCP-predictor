9535f31d05cfe6be6075ac132c1b6ef87cd8f9bb
0xnior <162879150+0xnior@users.noreply.github.com>
2024-10-04 20:41:24 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
-from flask import Flask, render_template, redirect, request,jsonify
+import streamlit as st
 import pickle
 import pandas as pd
-import numpy as np
 
-pipe = pickle.load(open("PycharmProjects/pythonProject1/IEX_model.pickle", 'rb'))
+st.markdown("<h1 style='text-align: center; font-size: 40px;'>MCP Predictor (Rs/MWh)</h1>", unsafe_allow_html=True)
+
+
+# Load the model and scaler
+pipe = pickle.load(open("IEX_model.pickle", 'rb'))
 scaler = pickle.load(open("scaler.pickle", 'rb'))
 
-app = Flask(__name__)
+# Define the form
+with st.form("my_form"):
+    # Input fields
+    d = {
+        'Purchase': [st.number_input("Purchase")],
+        'sbTotal': [st.number_input("sbTotal")],
+        'sbSolar': [st.number_input("sbSolar")],
+        'sbNonSolar': [st.number_input("sbNonSolar")],
+        'sbHydro': [st.number_input("sbHydro")],
+        'mcvTotal': [st.number_input("mcvTotal")],
+        'mcvNonsolar': [st.number_input("mcvNonsolar")],
+        'mcvHydro': [st.number_input("mcvHydro")],
+        'fsvTotal': [st.number_input("fsvTotal")]
+    }
 
+    # Submit button inside the form
+    submitted = st.form_submit_button(label='Predict')
+    # Perform prediction after form submission
+    if submitted:
+        # Convert the dictionary to a DataFrame
+        df = pd.DataFrame(d)
 
-@app.route('/', methods=['POST', 'GET'])
-def home_page():
+        # Scale the input data and make a prediction
+        prediction = pipe.predict(scaler.transform(df))
 
-    return render_template('index.html',
-                           prediction=-1)
-
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get the input data from the request
-    data = request.json  # Expecting JSON format
-    df = pd.DataFrame(data, index=[0])  # Convert to DataFrame
-    return jsonify(prediction=pipe.predict(scaler.transform(df)).tolist())  # Return the prediction as JSON
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        # Display the result
+        st.markdown(f"<h1 style='text-align: center; font-size: 40px;'>Prediction: {prediction[0]}</h1>",
+                    unsafe_allow_html=True)
